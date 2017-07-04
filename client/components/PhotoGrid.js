2ac18bb481bebcb9ab37fd9cc72559e34a3afdee
Kelly T Le <kellytnle@gmail.com>
2017-07-03 18:33:04 -0700

    ADDED: start components

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PhotoGrid extends Component {
+  render() {
+    return (
+      <div>PhotoGrid Component</div>
+    );
+  }
+}
+
+export default PhotoGrid;
