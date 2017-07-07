1dae37d0620768609da7dfe22f7dc4894ed73864
Kelly T Le <kellytnle@gmail.com>
2017-07-06 22:46:29 -0700

    hot reload reducers

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,12 +4,30 @@ import css from './styles/style.styl';
 import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
-
 // import react router dependencies 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import { Provider } from 'react-redux';
 import store, { history } from './store';
+
+/**Sentry
+import Raven from 'raven-js';
+import { sentry_url, logException } from './data/config';
+
+Raven.config(sentry_url, {
+  tags: {
+    git_commit: 'asfdsafds',
+    userLevel: 'editor'
+  }
+}).install();
+
+logException(new Error('download Failed!'), {
+  email: 'kelly@gmail.com'
+});
+
+Raven.captureMessage('Something bad happened!'); 
+Raven.showReportDialog();
+**/
 
 const router = (
   <Provider store={store}>
