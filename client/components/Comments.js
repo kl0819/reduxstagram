53c4b2dab68b2d863685a6fd932c43af921bb5ab
Kelly T Le <kellytnle@gmail.com>
2017-07-06 22:02:03 -0700

    adding and removing comment state

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class Comments extends Component {
+  constructor(props) {
+      super(props);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.renderComment = this.renderComment.bind(this);
+  }
+
   renderComment(comment, i) {
     return (
       <div className="comment" key={i}>
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button onClick={this.props.removeComment.bind(null, this.props.params.postId, i)} className="remove-comment">&times;</button>
         </p>
       </div>
     );
@@ -26,12 +32,11 @@ class Comments extends Component {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commmentForm" className="comment-form" onSubmit={this.handleSubmit}>
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author"/>
           <input type="text" ref="comment" placeholder="comment"/>
-          <input type="submit" hidden/>
+          <input type="submit" hidden />
         </form>
-
       </div>
     );
   }
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,33 @@
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      // return new state with new comment 
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      return [
+        // From the start to the one we want to delete 
+        ...state.slice(0, action.i),
+        // after the deleted one, to the end of array
+        ...state.slice(action.i + 1)
+      ];
+    default: 
+      return state;
+  }
+  return state; 
+}
+
 function comments(state = [], action) {
-  console.log(state, action);
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // take current state 
+      ...state,
+      // overwrite post with a new one 
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   return state; 
 }
 
